a233f238f2ac5171f7633f743e7bad957c0faa86
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-01-28 17:48:53 +0300

    Java variables identifiers

--- Variables.java
+++ Variables.java
@@ -1,14 +1,25 @@
 /*
- * =====      Java Variables  =====
- * ~ variable is a container for storing data values.
+ **  =====      Java Variables  =====
+ *~ variable is a container for storing data values.
  * Types of variables: 
  *      - String: store text value. i.e:. "value" 
  *      - int: stores integers (whole numbers) without decimal point. i.e:. 2, 67, 894, -45
  *      - float/ double: stores real number with decimal points. i.e:. 19.9 or -2.4
  *      - char: stores single characters. i.e:. 'a', 'B' --> Char values are surrounded by single quotes
  *      - boolean: stores values with two states; True, False
+ *~
  * Syntax:.
  *      type variableName = value;
+ * 
+ * 
+ *  The general rules for naming variables are:
+        Names can contain letters, digits, underscores, and dollar signs
+        Names must begin with a letter
+        Names should start with a lowercase letter and it cannot contain whitespace
+        Names can also begin with $ and _ (but we will not use it in this tutorial)
+        Names are case sensitive ("myVar" and "myvar" are different variables)
+        Reserved words (like Java keywords, such as int or boolean) cannot be used as names
+ *
  * 
 */
 
@@ -23,7 +34,7 @@ public class Variables
 
         // integer value
         int myNumber = 56;
-        
+
         // replace myNumber with new value
         myNumber = 34;
         System.out.println(myNumber);
