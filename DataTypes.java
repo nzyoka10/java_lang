568b33860d7518563b72ceca7e8109c3ce68f406
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-01-28 18:02:00 +0300

    java data types

---
+++ DataTypes.java
@@ -0,0 +1,34 @@
+// Java Data Types
+// a variable in Java must be a specified data type
+/**
+ * ~ Example below
+ * *       int myNum = 6; // integer
+ * &      float myFloatNum = 3.14f; // floating point number
+ * ^      char myLetter = 'a'; // character (single quotes)
+ * !        boolean isItTrue = true; // Boolean, can only have values of true or false
+ * *        String myText = "hello world"; // String
+ * 
+*/
+
+public class DataTypes
+{
+    public static void main(String[] args)
+    {
+        int num = 7;
+        System.out.println("This is an integer: " + num);
+        
+        float fltNum = 2.5f;
+        System.out.println("\nThis is a float: " + fltNum);
+
+        char letter = 'A';
+        System.out.println("\n this is a letter: " + letter);
+
+        String name ="Eric Nzyoka";
+        System.out.println("\n String name: " + name);
+
+        boolean isTall = false;
+        System.out.println("\n this is a boolean: " + isTall);
+        
+
+    }
+}
