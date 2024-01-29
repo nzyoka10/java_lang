ab836cb17d135c2f323866448df6a2fcf211c630
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-01-29 09:59:19 +0300

    update: java comaprison operators

--- Operators.java
+++ Operators.java
@@ -13,8 +13,8 @@ public class Operators
      *  Java divides the operators into the following groups:
     **            Arithmetic operators ----> +, -, *, /, %, ++, --, 
     **            Assignment operators ---> =, +=, -=, *=, %=, /=, &=, |=, ^=, >>=, <<=
-    **            Comparison operators
-    **            Logical operators
+    **            Comparison operators ---> !=, <, >, <=, >=, ==
+    **            Logical operators  --->  
     **            Bitwise operators
             
             
@@ -22,6 +22,51 @@ public class Operators
 
     public static void main(String[] args)
     {
+        //* Arithmetic operations */
+        int a = 4;
+        int b = 8;
+        int sum = a + b;
+        int multiply = a * b;
+        int division = sum / b;
+        int modulus = a % b;
+        int increment = ++sum;
+        int decrement = sum--;
+
+        System.out.println("Sum of a and b is: " + sum + ""); //~ Output: 12
+      /**
+       *  System.out.println("Multiply a and b: " + multiply + ""); //~ Output: 32
+            System.out.println("Divide sum and b: " + division + ""); //~ Output: 1
+            System.out.println("Modulus of a and b is: " + modulus + ""); //~ Output: 4
+            System.out.println("Increment of sum: " + increment + ""); //~ Output: 13
+            System.out.println("Decrement of sum: " + decrement + ""); //~ Output: 11
+        **/
+        //System.out.println("\n");
+
+        //** Java assignment operators */
+        // used to assign values to variables 
+            int myNum = 3;
+            System.out.println(myNum); // output: 3
+
+        // addition assignment operator (+=), adds a value to a variable
+            int myNum1 = 10;
+            myNum1 += 5;
+            System.out.println(myNum1); // Output: 15
+
+        //~ -------------   Comparison Operators -----------------
+            // used to compare two variables 
+            int myNum2 = 10;
+            int myNum3 = 7;
+            System.out.println(myNum2 > myNum3); // Returns: TRUE , 10>7
+        
+        //
+
+        
+
+
+
+
+
+
         int x = -4;
         System.out.println(x>>1); // Output: -2
 
