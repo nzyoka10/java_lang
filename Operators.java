b50027866139746574a250e31579907d09d086e9
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-01-29 09:30:26 +0300

    java operators

---
+++ Operators.java
@@ -0,0 +1,32 @@
+public class Operators 
+{
+    /**
+     * *    Operators : used to perform operations on variables and values
+     *              + :  used to add two values
+     *                  : + operator is often used to add together two values, 
+     *~                  Eg:- , it can also be used to add together a variable and a value, 
+     * ~                          or a variable and another variable:
+     * &                              int sum1 = 100 + 50;        // 150 (100 + 50)
+     * &                              int sum2 = sum1 + 250;      // 400 (150 + 250)
+     * &                              int sum3 = sum2 + sum2;     // 800 (400 + 400)
+     *  
+     *  Java divides the operators into the following groups:
+    **            Arithmetic operators ----> +, -, *, /, %, ++, --, 
+    **            Assignment operators ---> =, +=, -=, *=, %=, /=, &=, |=, ^=, >>=, <<=
+    **            Comparison operators
+    **            Logical operators
+    **            Bitwise operators
+            
+            
+     */
+
+    public static void main(String[] args)
+    {
+        int x = -4;
+        System.out.println(x>>1); // Output: -2
+
+        int y = 7;
+        System.out.println(y>>1); // output: 3
+
+    }
+}
