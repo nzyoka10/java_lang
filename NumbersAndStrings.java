98c19034e7368f427aa7a3223395c2b64589b1da
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-01-29 12:16:18 +0300

    java numbers and string operations

---
+++ NumbersAndStrings.java
@@ -0,0 +1,31 @@
+public class NumbersAndStrings 
+{
+    /**
+     * *     ********------  Adding Numbers and Strings  -------- ***********************
+     * !     Java uses the + operator for both addition and concatenation
+     * ~           That is:-
+     * ~                 --> Numbers are added together and,
+     * ~                 --> Strings are added together
+     **        //    If you add two numbers, the result will be a number:
+     * ~ --> int x=1, y= 2; int z=x+y;    //! z will be an integer, 3 
+     */
+    public static void main(String[] args)
+    {
+        //~ If you add two numbers, the result will be a number.
+            int x = 10, y = 20;
+            int z = x + y; 
+            System.out.println(z);  // Output: integer
+        
+        //! if you add two strings, the result will be a string concatenation
+            String x1 = "10", y1 = "20";
+            String z1 = x1 + y1;
+            System.out.println(z1);  // Output: String
+
+        //& if you add a number and a string, the result will be a string concatenation
+            String x2 = "KY100"; int y2 = 20;
+            String z2 = x2 + y2;
+            System.out.println(z2);
+
+        //
+    }
+}
