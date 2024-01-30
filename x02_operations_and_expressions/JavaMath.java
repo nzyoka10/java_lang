e1c77e22292f1b68dd2de2b93e0a8a4684304397
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-01-30 08:25:41 +0300

    java math

---
+++ x02_operations_and_expressions/JavaMath.java
@@ -0,0 +1,12 @@
+public class JavaMath 
+{
+    public static void main(String[] args)
+    {
+        int x = 5, y = 7;
+        System.out.println(Math.max(x, y)); // returns  the maximum of x and y
+        System.out.println(Math.sqrt(70)); // returns  the square root of a number
+        System.out.println(Math.abs(-5.4)); // the Math.abs(x) method returns the absolute (positive) value of num
+        System.out.println(Math.random()); // the Math.random() returns a random number between 0.0 (inclusive), and 1.0 (exclusive)
+
+    }
+}
