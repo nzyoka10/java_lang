93fb83d4863d06e528b98386c2518b1477966ecf
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-01-30 08:41:20 +0300

    update: more of java boolean expressions

--- x02_operations_and_expressions/Boolean.java
+++ x02_operations_and_expressions/Boolean.java
@@ -5,7 +5,28 @@ public class Boolean
         boolean isJavaFun = true;
         boolean isFishTasty = false;
 
-        System.out.println(isJavaFun);
-        System.out.println(isFishTasty);
+        System.out.println(isJavaFun); // true
+        System.out.println(isFishTasty); // false
+
+        //~ Boolean Expression
+            // returns a boolean value: true or false
+            int x = 10, y = 9;
+            System.out.println(x > y);
+            System.out.println(x < y);
+            System.out.println(y == 9);
+        
+        //^ Example:-
+        int myAge = 27;
+        int votingAge = 18;
+            if (myAge >= votingAge)
+            {
+                System.out.println("Allowed to Vote!");
+            }
+            else
+            {
+                System.out.println("Not Allowed to Vote!");
+            }
+       // System.out.println(myAge >= votingAge); // allowed to vote
+
     }
 }
