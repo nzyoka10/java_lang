fbd0b4650d743247263123f017788c824dca0f0f
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-01-30 09:00:55 +0300

    conditional statements in java

---
+++ x03_control_Flow_Structures/Main.java
@@ -0,0 +1,15 @@
+package x03_control_Flow_Structures;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        int x = 5;
+        int y = 6;
+
+        if (x > y)
+        {
+            System.out.println("x is greater than y");
+        }
+    }
+}
